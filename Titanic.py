7abaf5dd0b6ce20408d71cf1944b3028a1f48b08
Georgios Drakos <george.drakos62@gmail.com>
2018-08-10 23:24:18 +0100

    Update Titanic.py

--- Titanic.py
+++ Titanic.py
@@ -204,10 +204,10 @@ submission.head()
 # within acceptable limit, otherwise I would have trained my model with the reduced dataset.
 
 ##### Solution with stacking : just comment everything betwwen line 183 to 201 and uncomment the following commands
-import stacking
-pred = stacking.model(data[:train_objs_num].values, y.ravel(),data[train_objs_num:].values)
-print(pred)
-submission = pd.DataFrame({ 'PassengerId': test.passengerid,
-                            'Survived': pred })
-submission.to_csv("submission.csv", index=False)
-submission.head()
+#import stacking
+#pred = stacking.model(data[:train_objs_num].values, y.ravel(),data[train_objs_num:].values)
+#print(pred)
+#submission = pd.DataFrame({ 'PassengerId': test.passengerid,
+#                            'Survived': pred })
+#submission.to_csv("submission.csv", index=False)
+#submission.head()
